6eba5cfd2c1c947a5598faf83e7f1cc7a7ca7c90
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-18 15:32:34 -0500

    feat(app): Add string interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-store';
+  name = 'Nikola';
+  age = 21;
+  imgURL = 'https://source.unsplash.com/random';
 }

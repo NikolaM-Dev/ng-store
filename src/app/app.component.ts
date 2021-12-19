2f6d92abf09a24db4879bb019dda06edb96bc0da
Juan David Merchan Torres <juan1014166@gmail.com>
2021-12-19 12:47:00 -0500

    feat(app): Add ngClass & ngStyle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,11 @@ export class AppComponent {
   widthImg = 100;
   names: string[] = ['Nicolas', 'Julian', 'Santi'];
   newName: string = '';
+  box = {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  };
   products: Product[] = [
     {
       name: 'EL mejor juguete',
